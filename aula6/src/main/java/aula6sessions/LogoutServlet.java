1574f3dd62817dd7dddc0ee3d1da231bdd0116d4
rafaelgurk <rafaelgurk@gmail.com>
2024-03-17 12:58:01 -0300

    update aula6

--- aula6/src/main/java/aula6sessions/LogoutServlet.java
+++ aula6/src/main/java/aula6sessions/LogoutServlet.java
@@ -18,7 +18,7 @@ public class LogoutServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 						 HttpServletResponse response) throws ServletException, IOException {
 
-		response.setContentType("text/html; charset=UTF-8");
+		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		
 		request.getRequestDispatcher("link.html").include(request, response);
